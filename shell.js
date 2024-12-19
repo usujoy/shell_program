1e074c8458b7a5b56b31ae2c056a5e13b1529ea6
usujoy <183721610+usujoy@users.noreply.github.com>
2024-12-19 21:10:37 +0530

    added function for make directory

--- shell.js
+++ shell.js
@@ -1,26 +1,42 @@
-let currentDirectory = '~';
+const currentDirectory = ['~'];
+const directories = [];
 const shellName = 'shell v2.0 ';
 const symbol = ' % ';
 const listOfFiles = [];
 
 const cd = function (args) {
-  currentDirectory = args.join();
-  return shellName + currentDirectory + symbol;
-}
+  if (directories.includes(args.slice(-1).join( ))) {
+    currentDirectory.push(args.join(' '));
+    
+    const path = shellName + (currentDirectory.join('/') )+ symbol;
+    return path;
+  }
+
+  console.log('No such directories');
+};
 
 const pwd = function () {
   return currentDirectory;
-}
+};
 
 const ls = function () {
   console.log(listOfFiles.join(' '));
   return;
-}
+};
 
 const touch = function  (args) {
   const newFile = args.join();
 
   listOfFiles.push(newFile);
+};
+
+const makedirectory = function (args) {
+  if (args.includes(' ')) {
+    console.log('use camelcase');
+    return;
+  }
+
+  directories.push(args.join());
 }
 
 const externalCommands = function (commandString) {
@@ -29,31 +45,37 @@ const externalCommands = function (commandString) {
   switch (command) {
     case 'touch': 
       return touch(args);
-      
-  
+    case 'mkdir':
+      return makedirectory(args);
     default:
       console.log('shell v2.0 command not found');
   }
-}
+};
+
+const echo = function (args) {
+  console.log(args.join(' '));
+};
 
 const runCommand = function (commandString) {
   const [command, ...args] = commandString.split(' ');
 
   switch (command) {
     case 'cd':
-      return cd(args)
+      return cd(args);
     case 'pwd':
       console.log(pwd());
       return;
     case 'ls':
       return ls();
+    case 'echo':
+      return echo(args);
     default:
       return externalCommands(commandString);
   }
-}
+};
 
 while (true) {
-  const command = prompt(shellName + currentDirectory + symbol);
+  const command = prompt(shellName + currentDirectory.join('/') + symbol);
 
   const runningCommand = runCommand(command);
 }
