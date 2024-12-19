c6b8db8c876f6c49a889a2481818396827f43ba0
usujoy <183721610+usujoy@users.noreply.github.com>
2024-12-19 15:41:20 +0530

    added new functions in shell.js

--- shell.js
+++ shell.js
@@ -1,1 +1,43 @@
-const promptMessage = 'shell v2.0';
+const promptMessage = 'shell v2.0 % ';
+let currentDirectory = '~ ';
+
+const cd = function (...args) {
+  currentDirectory = args;
+  return promptMessage + currentDirectory;
+}
+
+const pwd = function () {
+  return currentDirectory;
+}
+
+const ls = function () {
+  return;
+}
+
+const externalCommands = function (command) {
+  console.log('shell v2.0 command not found');
+}
+
+const runCommand = function (commandString) {
+  const [command, ...args] = commandString.split(' ');
+  console.log(command);
+  
+
+  switch (command) {
+    case 'cd':
+      return cd(args)
+    case 'pwd':
+      console.log(pwd())
+      return;
+    case 'ls':
+      return ls();
+    default:
+      return externalCommands(command);
+  }
+}
+
+while (true) {
+  const command = prompt(promptMessage + currentDirectory);
+
+  const runningCommand = runCommand(command);
+}
